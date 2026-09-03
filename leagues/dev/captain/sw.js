1ae0c439760c82447e2052471bbcf815766c39f0
mhx-labs <ptinc.rh@gmail.com>
2026-09-03 21:43:05 +0100

    chore(pwa): bump leagues dev service worker caches to v3

--- leagues/dev/captain/sw.js
+++ leagues/dev/captain/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-captain-dev-cache-v2';
+const CACHE_NAME = 'ff-captain-dev-cache-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
--- leagues/dev/club/sw.js
+++ leagues/dev/club/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-club-dev-cache-v2';
+const CACHE_NAME = 'ff-club-dev-cache-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
