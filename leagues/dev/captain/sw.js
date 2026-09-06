2b0ecd498e6d1d96aa001842565a48c5c1951899
mhx-labs <ptinc.rh@gmail.com>
2026-09-06 03:27:34 +0100

    feat(ddlc-dev): deploy PWA updates

--- leagues/dev/captain/sw.js
+++ leagues/dev/captain/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-captain-dev-cache-v45';
+const CACHE_NAME = 'ff-captain-dev-cache-v46';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
--- leagues/dev/club/sw.js
+++ leagues/dev/club/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-club-dev-cache-v45';
+const CACHE_NAME = 'ff-club-dev-cache-v46';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
