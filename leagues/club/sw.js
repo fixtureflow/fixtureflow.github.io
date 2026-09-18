42680ed358382a11b7e98b32f86a694b6a657cf7
mhx-labs <ptinc.rh@gmail.com>
2026-09-18 14:22:31 +0100

    feat(ddlc): deploy production PWA v3.1.0

--- leagues/club/sw.js
+++ leagues/club/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-club-cache-v3.0.0';
+const CACHE_NAME = 'ff-club-cache-v3.1.0';
 const CACHE_PREFIX = 'ff-club-cache-';
 const ASSETS_TO_CACHE = [
   './',
--- leagues/player/sw.js
+++ leagues/player/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-player-cache-v3.0.0';
+const CACHE_NAME = 'ff-player-cache-v3.1.0';
 const CACHE_PREFIX = 'ff-player-cache-';
 const ASSETS_TO_CACHE = [
   './',
