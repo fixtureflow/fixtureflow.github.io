49352dcfd19b7000b40846a7c54cf1fe8ab4459c
mhx-labs <ptinc.rh@gmail.com>
2026-09-21 19:54:17 +0100

    feat(courtflow-dev): deploy PWA updates

--- courtflow/dev/sw.js
+++ courtflow/dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ff-courtflow-dev-cache-v78';
+const CACHE_NAME = 'ff-courtflow-dev-cache-v79';
 const CACHE_PREFIX = 'ff-courtflow-dev-cache-';
 const ASSETS_TO_CACHE = [
   './',
