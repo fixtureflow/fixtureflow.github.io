280d8568e508572371b84eacb81950f6d5b41e91
mhx-labs <ptinc.rh@gmail.com>
2026-09-25 03:29:11 +0100

    feat(courtpay-dev): deploy PWA updates

--- courtpay/dev/sw.js
+++ courtpay/dev/sw.js
@@ -1,5 +1,5 @@
 // CourtPay Standalone Service Worker (dev - v1.0.0)
-const CACHE_NAME = 'courtpay-dev-v1.0.0-1790301468719';
+const CACHE_NAME = 'courtpay-dev-v1.0.0-1790303351831';
 const ASSETS = [
   '/courtpay/dev/',
   '/courtpay/dev/index.html',
